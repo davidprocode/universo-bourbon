29c27a6b733c0a4906068a26575db48b2372ac32
davidprocode <davidprocode@proton.me>
2023-11-04 10:06:48 -0300

    add video to background

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,23 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, useRef } from 'react-native';
+import * as React from 'react';
+import { Video, ResizeMode } from 'expo-av';
+import BackgroundVideo from '../../../assets/background.mp4'
 
 export default function Home({navigation}) {
+  const video = React.useRef(null);
+  video.current.playAsync()
+
   return (
-    <View style={styles.container}>
-      <Text>Seja bem vindo ao Universo Bourbon!</Text>
+    <View style={styles.container}>      
+      <Video
+        ref={video}
+        style={styles.backgroundVideo}
+        source={BackgroundVideo}
+        isLooping={true}
+        volume={0}
+        resizeMode={ResizeMode.COVER}
+      />
+      <Text style={styles.title}>Seja bem vindo ao Universo Bourbon!</Text>      
       <TouchableOpacity 
       style={styles.button}
       onPress={() =>navigation.navigate('About', {name: 'Jane'})}
@@ -29,7 +43,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#0859c6',
     padding: 10,
   },
-  text:{
-    color: '#fff'
-  }
+  title:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    width:'80%',
+    color: '#fff',
+    fontSize: 50,
+    title: 'center',
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+
 });
