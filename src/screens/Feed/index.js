917bd01d081df16d0bbf4564965a0f98723d6c20
davidprocode <davidprocode@proton.me>
2023-11-06 18:51:55 -0300

    bug on video

--- src/screens/Feed/index.js
+++ src/screens/Feed/index.js
@@ -1,10 +1,35 @@
 import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
 import { SafeAreaFrameContext } from "react-native-safe-area-context";
+import { Video, ResizeMode } from 'expo-av';
+import { useRef, useState } from 'react';
+
 
 export default function Feed() {
-  const data = [1, 2, 3, 4, 5];
+  const data = [
+    {
+      id :1,
+      title :'Video 01',
+      Description :'Description Video 01',
+      url :'https://i.imgur.com/zkZiIrW_lq.mp4',
+    },
+    {
+      id :2,
+      title :'Video 02',
+      Description :'Description Video 02',
+      url :'https://i.imgur.com/gP8hngA_lq.mp4',
+    },
+    {
+      id :3,
+      title :'Video 03',
+      Description :'Description Video 03',
+      url :'https://i.imgur.com/WXFwBwS_lq.mp4',
+    },
+  ];
 
   const renderItem = ({ item, index }) => {
+    const video = useRef(null);
+    const [status, setStatus] = useState({});
+
     return (
       <View
         style={[
@@ -19,13 +44,26 @@ export default function Feed() {
             : { backgroundColor: "pink" },
         ]}
       >
-        <Text>Hello from item{item}</Text>
+        <Text style={{color:"#fff", fontWeight: "bold"}}>{item.title}</Text>
+        <Text style={{color:"#fff", }}>{item.Description}</Text>
+        <Video
+        ref={video}
+        source={{
+          uri: item.url,
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
       </View>
     );
   };
+
   return (
     <View style={styles.container}>
       <FlatList
+        key={data.id}
         data={data}
         renderItem={renderItem}
         pagingEnabled
