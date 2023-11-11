be72eb2facadc2ad10a18ccf3b5210927498858a
davidprocode <davidprocode@proton.me>
2023-11-11 12:26:25 -0300

    fix text position

--- src/screens/Feed/FeedItem.js
+++ src/screens/Feed/FeedItem.js
@@ -1,23 +1,40 @@
 import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
-import { Video, ResizeMode } from 'expo-av';
+import { Video, ResizeMode } from "expo-av";
 
-
-export default function FeedItem({ item, index }){
-    return (
+export default function FeedItem({ item, index }) {
+  return (
+    <View
+      style={[
+        {
+          flex: 1,
+          height: Dimensions.get("window").height,
+          width: "100%",
+        },
+        index % 2 == 0
+          ? { backgroundColor: "blue" }
+          : { backgroundColor: "pink" },
+      ]}
+    >
       <View
-        style={[
-          { 
-            flex: 1,
-            height: Dimensions.get('window').height,
-            width: '100%',
-          },
-          index % 2 == 0
-            ? { backgroundColor: "blue" }
-            : { backgroundColor: "pink" },
-        ]}
+        style={{
+          flex: 1,
+          backgroundColor: "green",
+          position: "absolute",
+          padding: 25,
+          bottom: 100,
+        }}
       >
-        <Text style={{color:"#fff", fontWeight: "bold"}}>{item.title}</Text>
-        <Text style={{color:"#fff"}}>{item.Description}</Text>
+        <Text
+          style={{
+            color: "#fff",
+            fontWeight: "bold",
+            fontSize: 20,
+          }}
+        >
+          {item.title}
+        </Text>
+        <Text style={{ color: "#fff", fontSize: 16 }}>{item.Description}</Text>
       </View>
-    )
-  };
+    </View>
+  );
+}
